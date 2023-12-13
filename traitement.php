a7db2e124f0f487f4eea93e8d1625450b7e11d22
AnthonyCarmichael <anthonycarmichael0@gmail.com>
2023-12-12 22:31:57 -0500

    J'avais oublié de gèrer l'affichage connexion dans traitement

--- traitement.php
+++ traitement.php
@@ -41,29 +41,32 @@
             }
             else {
                 ?>
-                <h2 class="center">Entrez votre utilisateur et mot de passe <br> pour accéder aux fonctionnalités</h2>
-                <p class="white center">
-                    Le nom d'utilisateur ou le mot de passe ne correspond.
-                </p>
+                <section id="connexion">
 
-                <form action="traitement.php" method="post" class="login">
+                    <h2 class="center">Entrez votre utilisateur et mot de passe <br> pour accéder aux fonctionnalités</h2>
+                    <p class="white center">
+                        Le nom d'utilisateur ou le mot de passe ne correspond.
+                    </p>
 
-                    <div class="row">
-                        <label class="white" for="username">Nom d'utilisateur: </label>
-                        <input type="text" name="username" id="username">
+                    <form action="traitement.php" method="post" class="login">
+
+                        <div class="row">
+                            <label class="white" for="username">Nom d'utilisateur: </label>
+                            <input type="text" name="username" id="username">
+                            </div>
+
+                        <div class="row">
+                            <label class="white" for="mdp">Mot de passe: </label>
+                            <input type="password" name="mdp" id="mdp">
                         </div>
 
-                    <div class="row">
-                        <label class="white" for="mdp">Mot de passe: </label>
-                        <input type="password" name="mdp" id="mdp">
-                    </div>
+                        <input type="hidden" name="action" value="connexion">
 
-                    <input type="hidden" name="action" value="connexion">
-
-                    <div class="row">
-                        <button type="submit">Se connecter</button>
-                    </div>
-                </form>
+                        <div class="row">
+                            <button type="submit">Se connecter</button>
+                        </div>
+                    </form>
+                </section>
                 <?php
             }
 
