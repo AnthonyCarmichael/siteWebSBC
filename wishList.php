6d42ea793c7c7d0cacddc52fc9e4ac31340c60da
Maxime Malette <201650050@cegepsherbrooke.qc.ca>
2023-12-14 13:09:15 -0500

    pre pull main richard cookie

--- wishList.php
+++ wishList.php
@@ -1,4 +1,4 @@
-<?php include_once("inc/header.php"); 
+<?php include_once("inc/header.php");
 $bdd = PDOFactory::getMySQLConnection();
 $sm = new SBCManager($bdd);
 print_r($_SESSION['souhait']);
@@ -6,10 +6,14 @@ print_r($sm->getSBCById($_SESSION['souhait'][1])); ?>
 <h2 class="center">Votre liste de souhait</h2>
 
 <?php $souhait = $_SESSION['souhait'];
- foreach ($souhait as $sbc){
-    
-    echo $sm->getSBCById($sbc)->get_marqueSBC();
-    echo $sm->getSBCById($sbc)->get_modele();
-    echo $sm->getSBCById($sbc)->get_prix();
- } ?>
+foreach ($souhait as $sbc) { ?>
+    <div><a href="img/<?= $sm->getSBCById($sbc)->get_modele(); ?>.jpg"><img
+                src="img/<?= $sm->getSBCById($sbc)->get_modele(); ?>.jpg"
+                alt="<?= $sm->getSBCById($sbc)->get_modele(); ?>.jpg"></a>
+        <?php echo $sm->getSBCById($sbc)->get_marqueSBC();
+        echo $sm->getSBCById($sbc)->get_modele();
+        echo $sm->getSBCById($sbc)->get_prix(); ?>
+    </div>
+
+<?php } ?>
 <?php include_once("inc/footer.php"); ?>
