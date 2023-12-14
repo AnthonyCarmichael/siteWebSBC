0f43f5b48d2d2daf35aaae640c1496934db6c913
Maxime Malette <201650050@cegepsherbrooke.qc.ca>
2023-12-14 12:56:16 -0500

    Début page wishlist

--- classe/souhait.php
+++ classe/souhait.php
@@ -1,15 +1,30 @@
 <?php
-class souhait{
-    public function __construct(){
-        if(!isset($_SESSION)){
+class souhait
+{
+    public function __construct()
+    {
+        if (!isset($_SESSION)) {
             session_start();
         }
-        if(!isset($_SESSION['souhait'])){
+        if (!isset($_SESSION['souhait'])) {
             $_SESSION['souhait'] = array();
         }
     }
 
-    public function add($SBC_id){
-        $_SESSION['souhait'][$SBC_id] = 1;
+    public function add($SBC_id)
+    {
+        $i = 0;
+        $idSbc = $_SESSION['souhait'];
+        $doublon = false;
+        foreach ($idSbc as $sbc) {
+            $i++;
+            if ($sbc == $SBC_id) {
+                $doublon = true;
+            }
+        }
+        if ($doublon == false) {
+            $_SESSION['souhait'][$i] = $SBC_id;
+        }
+
     }
 }
--- wishList.php
+++ wishList.php
@@ -1,2 +1,15 @@
-<?php include_once("inc/header.php"); ?>
+<?php include_once("inc/header.php"); 
+$bdd = PDOFactory::getMySQLConnection();
+$sm = new SBCManager($bdd);
+print_r($_SESSION['souhait']);
+print_r($sm->getSBCById($_SESSION['souhait'][1])); ?>
+<h2 class="center">Votre liste de souhait</h2>
+
+<?php $souhait = $_SESSION['souhait'];
+ foreach ($souhait as $sbc){
+    
+    echo $sm->getSBCById($sbc)->get_marqueSBC();
+    echo $sm->getSBCById($sbc)->get_modele();
+    echo $sm->getSBCById($sbc)->get_prix();
+ } ?>
 <?php include_once("inc/footer.php"); ?>
