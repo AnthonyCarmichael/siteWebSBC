07c7bd2dfa061a6156a92535c14cbcbe726d8aca
Maxime Malette <201650050@cegepsherbrooke.qc.ca>
2023-12-16 12:05:20 -0500

    petite modif historique

--- historique.php
+++ historique.php
@@ -24,8 +24,9 @@ $dateToday = new DateTime("now");
         $arrCommande = array_reverse($arrCommande);
         foreach ($arrCommande as $commande) { ?>
             <article id="commande">
-                <aside id="headCommande"> <?php
-                                            ?>
+                <aside id="headCommande">
+                    <?php
+                    ?>
                     <h3 class="center">Commande</h3>
                     <div class="flex">
                         <p class="white bold underline">Date de commande:</p>
@@ -35,28 +36,46 @@ $dateToday = new DateTime("now");
                         <p class="white bold underline">Numéro de commande</p>
                     </div>
                     <div class="flex">
-                        <p class="white"><?php echo $commande->get_date_commande(); ?></p>
-                        <p class="white"><?php echo $commande->get_prix() . "$"; ?></p>
-                        <p class="white"><?php echo $client->get_prenom() . " " . $client->get_nom(); ?></p>
-                        <p class="white"><?php echo $client->get_adresse() . " " . $client->get_ville() . " " . $client->get_province() . " " . $client->get_pays(); ?></p>
-                        <p class="white center"><?php echo $commande->get_id_commande(); ?></p>
+                        <p class="white">
+                            <?php echo $commande->get_date_commande(); ?>
+                        </p>
+                        <p class="white">
+                            <?php echo $commande->get_prix() . "$"; ?>
+                        </p>
+                        <p class="white">
+                            <?php echo $client->get_prenom() . " " . $client->get_nom(); ?>
+                        </p>
+                        <p class="white">
+                            <?php echo $client->get_adresse() . " " . $client->get_ville() . " " . $client->get_province() . " " . $client->get_pays(); ?>
+                        </p>
+                        <p class="white center">
+                            <?php echo $commande->get_id_commande(); ?>
+                        </p>
                     </div>
 
                 </aside>
                 <?php
                 $tabSBC = $commande->get_tabSBC();
                 //print_r($tabSBC);
-
+        
                 foreach ($tabSBC as $sbc) {
-                ?>
+                    ?>
                     <div id="infoSpecifique" class="flex">
                         <img src=<?php echo "img/" . $sbc->get_modeleSBC() . ".jpg"; ?> alt=<?php echo $sbc->get_modeleSBC(); ?>>
 
                         <div class="white">
-                            <p>Marque: <?php echo $sbc->get_marqueProcesseur(); ?></p>
-                            <p>Modèle: <?php echo $sbc->get_modeleProcesseur() ?></p>
-                            <p>Numéro d'identifiant: <?php echo  $sbc->get_id_SBC(); ?></p>
-                            <p>Prix: <?php echo $sbc->get_prix() . "$"; ?></p>
+                            <p>Marque:
+                                <?php echo $sbc->get_marqueProcesseur(); ?>
+                            </p>
+                            <p>Modèle:
+                                <?php echo $sbc->get_modeleProcesseur() ?>
+                            </p>
+                            <p>Numéro d'identifiant:
+                                <?php echo $sbc->get_id_SBC(); ?>
+                            </p>
+                            <p>Prix:
+                                <?php echo $sbc->get_prix() . "$"; ?>
+                            </p>
                         </div>
 
                         <form class="panier" action="traitement.php" method="get">
@@ -65,28 +84,42 @@ $dateToday = new DateTime("now");
                             <input type="submit" class="bouton" value="Ajouter au panier">
                         </form>
                     </div>
-                <?php
+                    <?php
                 }
 
                 $dateCommande = new DateTime($commande->get_date_commande());
                 $interval = $dateCommande->diff($dateToday);
-                if ($interval->format("%a") < 7) { ?>
+                if ($interval->format("%a") < 5) { ?>
                     <div class="end">
-                        <a class="bouton" href="traitement.php?action=delCommande&idCommande=<?php echo $commande->get_id_commande(); ?>">Il reste <?php echo 8 - $interval->format("%a"); ?> jours pour annuler la commande</a>
-                    </div><?php
-                        } elseif ($interval->format("%a") == 7) { ?>
+                        <a class="bouton"
+                            href="traitement.php?action=delCommande&idCommande=<?php echo $commande->get_id_commande(); ?>">Il reste
+                            <?php echo 7 - $interval->format("%a"); ?> jours pour annuler la commande
+                        </a>
+                    </div>
+                    <?php
+                } elseif ($interval->format("%a") == 5) { ?>
                     <div class="end">
-                        <a class="bouton" href="traitement.php?action=delCommande&idCommande=<?php echo $commande->get_id_commande(); ?>">Dernier jours pour annuler la commande</a>
-                    </div><?php
-                        }
-                            ?>
+                        <a class="bouton"
+                            href="traitement.php?action=delCommande&idCommande=<?php echo $commande->get_id_commande(); ?>">Dernier
+                            jour pour annuler la commande</a>
+                    </div>
+                    <?php
+                } elseif ($interval->format("%a") == 6) { ?>
+                    <div class="end">
+                        <a class="bouton"
+                            href="traitement.php?action=delCommande&idCommande=<?php echo $commande->get_id_commande(); ?>">Dernier
+                            jours pour annuler la commande</a>
+                    </div>
+                    <?php
+                }
+                ?>
 
             </article>
-        <?php
+            <?php
         }
     } else { ?>
         <h2 class="center">Vous n'avez pas effectué de commande</h2>
-    <?php
+        <?php
     }
 
     ?>
